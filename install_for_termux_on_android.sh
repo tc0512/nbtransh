50653ccfcac180931eff9e82f52ddac57c1356e5
tc0512 <tancheng_0812@qq.com>
2026-05-31 20:10:35 +0800

    添加termux下载脚本

---
+++ install_for_termux_on_android.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo -e "\033[32mVerifying environment...\033[0m"
+if command -v pkg >/dev/null 2>&1; then
+    echo "✓ Detected: Termux on Android"
+else
+    echo "::error::This script is not support this platform"
+fi
+echo -e "\033[32m[1/4] Install packages in isolated environment\033[0m"
+if command -v python >/dev/null 2>&1; then
+    echo "Python is already installed"
+else
+    echo "Python is not installed,so install it"
+    pkg update -y &>/dev/null
+    pkg install -y python 2>/dev/null 1>/dev/null
+fi
+if command -v git >/dev/null 2>&1; then
+    echo "git is already installed"
+else
+    echo "git is not installed,so install it"
+    pkg update -y &>/dev/null
+    pkg install -y git 2>/dev/null 1>/dev/null
+fi
+echo -e "\033[32m[2/4] Install runtime dependencies\033[0m"
+pkg update -y
+pkg install -y python-lxml
+if pip show sympy &>/dev/null; then
+    echo "translate is already installed"
+else
+    echo "translate is not installed,so install it"
+    pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ translate
+fi
+echo -e "\033[32m[3/4] Clone repository\033[0m"
+cd ~
+rm -rf nbtransh
+git clone https://github.com/tc0512/nbtransh.git
+rm nbtransh/*.sh
+echo -e "\033[32m[4/4] Set alias to launch faster\033[0m"
+echo "alias nbtransh='python /data/data/com.termux/files/home/nbtransh/nbtransh.py'" >> .bashrc
+source ~/.bashrc
+echo -e "\033[32mAll done!\033[0m"
+echo "The nbtransh work directory is /data/data/com.termux/files/home/nbtransh/"
+echo "Please donot remove it."
